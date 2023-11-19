349fc767809ef0cf2b32d6e69a8692e99ac90d04
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2023-11-18 20:05:53 -0500

    agregando navbar de bootstrap con Link de react

--- src/layouts/App.jsx
+++ src/layouts/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import NavBar from '../components/NavBar';
 import Home from '../pages/Home';
 import About from '../pages/About';
 import Contact from '../pages/Contact';
@@ -8,17 +9,8 @@ import '../assets/css/styles.css';
 
 const App = () => (
   <Router>
-    <nav>
-      <ul>
-        {/* Utiliza Link para crear enlaces */}
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">Acerca de</Link></li>
-        <li><Link to="/contact">Contacto</Link></li>
-      </ul>
-    </nav>
-    <hr />
+    <NavBar />
     <Routes>
-      {/* Rutas */}
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
