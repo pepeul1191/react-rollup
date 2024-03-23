d1b86e18908b6649e35b04388b19e9193e4dddf6
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2024-03-22 20:30:39 -0500

    maquetación de cambiar contraseña

--- src/pages/login/ResetPassword.jsx
+++ src/pages/login/ResetPassword.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Form, Button, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import LogoSVG  from './../../assets/svg/bBOV3E01.svg';
 
 const ResetPassword = () => {
   const [email, setEmail] = useState('');
@@ -14,35 +15,40 @@ const ResetPassword = () => {
 
   return (
     <>
-      <Form onSubmit={handleSubmit} style={{width: '300px'}} className="row" >
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit" className="w-100 mt-2">
-          Submit
-        </Button>
-      </Form>
+      <div className="form-container">
+        <div className="logo-container">
+          <LogoSVG className="logo custom-color" />
+        </div>
+        <Form onSubmit={handleSubmit} style={{width: '300px'}} className="row mt-2" >
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Correo</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Ingrese su correo"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit" className="w-100 mt-2">
+            Enviar Solicitud
+          </Button>
+        </Form>
+        <Row className="mt-2">
+          <Col>
+            <div className="left-aligned-content">
+              <Link to="/login" className="link" style={{ textAlign: 'left' }}>Ingresar</Link>
+            </div>
+          </Col>
+          <Col>
+            <div className="right-aligned-content" style={{'textAlign': 'right'}}>
+              <Link to="/sign-in" className="link" style={{ textAlign: 'right' }}>Crear cuenta</Link>
+            </div>
+          </Col>
+        </Row>
+      </div>
       <Row className="mt-2">
-        <Col>
-          <div className="left-aligned-content">
-            <Link to="/login" style={{ textAlign: 'left' }}>Ingresar</Link>
-          </div>
-        </Col>
-        <Col>
-          <div className="right-aligned-content" style={{'textAlign': 'right'}}>
-            <Link to="/sign-in" style={{ textAlign: 'right' }}>Crear cuenta</Link>
-          </div>
-        </Col>
-      </Row>
-      <Row className="mt-2">
-        <footer className="mt-3" style={{textAlign: 'left'}}>
-          Footer asasdf
+        <footer className="mt-3" style={{textAlign: 'left', paddingLeft: '14px'}}>
+          Powered by Software Web Perú 2023. <a style={{fontWeight: 400}} className="link" target="_blank" href="https://softweb.pe">Ir</a>.
         </footer>
       </Row>
     </>
