a19b675f2c061be2ac3e6d67a6a1ec74b4193a0b
Valdivia Caballero Jose Jesus <Jovaldiv@ulima.edu.pe>
2024-04-03 10:46:07 -0500

    vsta signin como copmenente y no funcion

--- src/pages/login/SignIn.jsx
+++ src/pages/login/SignIn.jsx
@@ -1,10 +1,65 @@
-import React from 'react';
+import React, { useState, Component } from 'react';
+import { Form, Button, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import LogoSVG  from './../../assets/svg/bBOV3E01.svg';
+import { sendEmail, loginCheck } from '../../services/UserService';
 
-const SignIn = () => (
-  <>
-    <h2>Crear Cuenta</h2>
-    <p>Esta es la página de información sobre nosotros.</p>
-  </>
-);
+class SignIn extends Component {
+  constructor(props) {
+    console.log('constructor')
+    super(props);
+    this.state = {
+      message: '',
+      messageClass: '',
+      disabled: false,
+      isValidJWT: false,
+    };
+    this.emailInputRef = React.createRef();
+  }
+
+  componentDidMount() {
+    const token = localStorage.getItem('jwtToken');
+    if (token != null) {
+      loginCheck()
+        .then(data => {
+          if (data.success == true){
+            // token time expired, continue to login
+            this.setState({
+              isValidJWT: true,
+            });
+          }
+        })
+        .catch(error => {
+          console.error("Error:", error);
+        });      
+    }
+  }
+
+  validateForm() {
+    
+    return false;
+  }
+
+  submit = (event) => {
+    event.preventDefault();
+    if (this.validateForm()) {
+      
+    }
+  };
+
+  render() {
+    const {message, messageClass, disabled, isValidJWT} = this.state;
+    if (isValidJWT) {
+      window.location.href = '/';
+      return null;
+    }
+    return (
+      <>
+        <h2>Crear Cuenta???</h2>
+        <p>Esta es la página de información sobre nosotros.</p>
+      </>
+    );
+  }
+}
 
 export default SignIn;
